82ebe1d89828ffa626dcddd5cb84af6d041eca6f
rohit20170 <rohitjangidrj17@gmail.com>
2024-06-18 16:45:46 +0530

    Add files via upload

---
+++ vite/src/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <main className="Main">
+      <h2>Welcome to My Website</h2>
+      <p>This is a simple React.js website example.</p>
+    </main>
+  );
+}
+
+export default Main;
